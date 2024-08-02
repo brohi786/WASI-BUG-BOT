043f7ddc3fa114205a31b5a114537c6a39868964
brohi786 <ahsanalibrohi126@gmail.com>
2024-08-02 21:52:22 +0500

    settings.js

--- settings.js
+++ settings.js
@@ -3,14 +3,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "923173611511"
+global.ownername = "TRY AFTER ME"
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
+global.ownernumber = '923173611511'  //creator number
+global.ownername = ' MR Ahsan ' //owner name
 global.botname = 'WASI TECH BUG BOT ' //name of the bot
 
 //sticker details
